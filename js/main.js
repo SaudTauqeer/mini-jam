4bdfdf72864b1127effdf29351df2030e8427250
Saud <saudtauqeer381@gmail.com>
2020-07-02 16:37:10 +0500

    added new dirs.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let isJamAllowed = false;
+
+const events = [
+  {
+    description: "on click event handler for attaching comment box.",
+    type: "",
+    handler: "",
+  },
+];
+
+function createCommentBox() {
+  const containerDiv = document.createElement("div");
+}
