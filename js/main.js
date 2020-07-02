7afda35037ec6cc36d764ce512df47aba307b999
Saud <saudtauqeer381@gmail.com>
2020-07-02 18:19:43 +0500

    added onClick event on window.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@
 const commentNewEl = document.getElementsByClassName("comment-new")[0];
 const clonedCommentNewEl = commentNewEl.cloneNode(true);
 
-console.log(clonedCommentNewEl, "yare");
-
 //for handling checkbox checked state on the parent div.
 const newCommentInputContainerEl = document.getElementsByClassName(
   "comment-new"
 );
 const commentCheckboxEl = document.getElementById("comment-checkbox");
+
+function handleAddNewCommentClick(e) {
+  console.log("alright", e);
+}
+
+window.onclick = handleAddNewCommentClick;
