1a40169966fbd9e148a4a55d1823c335ccd09f11
Saud <saudtauqeer381@gmail.com>
2020-07-02 18:07:10 +0500

    added more css and cleaned up main.js.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,2 @@
-let isJamAllowed = false;
-
-const events = [
-  {
-    description: "on click event handler for attaching comment box.",
-    type: "",
-    handler: "",
-  },
-];
-
-function createCommentBox() {
-  const containerDiv = document.createElement("div");
-}
+const newCommentInputContainer = document.getElementsByClassName("comment-new");
+const commentCheckbox = document.getElementById("comment-checkbox");
