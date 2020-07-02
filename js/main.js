ad8a8114787a171eb989308d8b4fa1dbbf1b4732
Saud <saudtauqeer381@gmail.com>
2020-07-02 18:16:49 +0500

    hook up more dom elements.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-const newCommentInputContainer = document.getElementsByClassName("comment-new");
-const commentCheckbox = document.getElementById("comment-checkbox");
+//cloned new comment element for appending later on.
+const commentNewEl = document.getElementsByClassName("comment-new")[0];
+const clonedCommentNewEl = commentNewEl.cloneNode(true);
+
+console.log(clonedCommentNewEl, "yare");
+
+//for handling checkbox checked state on the parent div.
+const newCommentInputContainerEl = document.getElementsByClassName(
+  "comment-new"
+);
+const commentCheckboxEl = document.getElementById("comment-checkbox");
